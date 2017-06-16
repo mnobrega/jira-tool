409e0a58b4ec4cd16482f35466b480618660e2af
Márcio Nóbrega <marcio.nobrega@premium-minds.com>
2017-06-16 16:08:55 +0100

    Final VagrantFile version with all configs necessary to run jira-tools.

--- .provision/bootstrap_jiratool.sh
+++ .provision/bootstrap_jiratool.sh
@@ -1,13 +1,5 @@
 #!/usr/bin/env bash
 
-#nginx
-sudo cp ~/code/jira-tool/.provision/nginx/nginx.conf /etc/nginx/sites-available/jiratools.conf
-sudo chmod 644 /etc/nginx/sites-available/jiratools.conf
-sudo ln -s /etc/nginx/sites-available/jiratools.conf /etc/nginx/sites-enabled/000-jiratools
-sudo mkdir /srv/www
-sudo ln -s ~/code/jira-tool/ /srv/www/jira-tool
-sudo service nginx start
-
 #app
 echo "Dependencies Install"
 cd ~/code/jira-tool
@@ -18,6 +10,15 @@ echo "DB Migrations"
 mysql -u homestead -psecret -e "CREATE DATABASE jira_tools;"
 /home/vagrant/code/jira-tool/vendor/bin/phinx migrate
 /home/vagrant/code/jira-tool/vendor/bin/phinx seed:run
+
+#nginx
+sudo cp ~/code/jira-tool/.provision/nginx/nginx.conf /etc/nginx/sites-available/jiratools.conf
+sudo chmod 644 /etc/nginx/sites-available/jiratools.conf
+sudo ln -s /etc/nginx/sites-available/jiratools.conf /etc/nginx/sites-enabled/000-jiratools
+sudo mkdir /srv/www
+sudo ln -s ~/code/jira-tool/ /srv/www/jira-tool
+#sudo service php7.1-fpm restart
+sudo service nginx restart
 
 ##### optional
  cat ~/code/jira-tool/.provision/ssh/mnobrega_key.pub >> ~/.ssh/authorized_keys
--- Vagrantfile
+++ Vagrantfile
@@ -72,7 +72,7 @@ Vagrant.configure("2") do |config|
   # Puppet, Chef, Ansible, Salt, and Docker are also available. Please see the
   # documentation for more information about their specific syntax and use.
   config.vm.provision "shell", privileged:false, run: 'once', path: ".provision/bootstrap.sh"
-  #config.vm.provision "shell", privileged:false, run: 'once', path: ".provision/bootstrap_jiratool.sh"
+  config.vm.provision "shell", privileged:false, run: 'once', path: ".provision/bootstrap_jiratool.sh"
   config.vm.provision "shell", privileged:false, run: 'always', inline: <<-SHELL
 	sudo install-keymap pt
   SHELL
